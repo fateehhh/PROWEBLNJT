9c05c54806a3fa1850e429b6ff5e89f4511cf3c5
fateehhh <siyaapppp@gmail.com>
2025-03-15 14:20:14 +0700

    JS5_Praktikum 3 – Membuat form kemudian menyimpan data dalam database

---
+++ Week 5/PWL_POS/resources/views/kategori/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('subtitle', 'Kategori')
+@section('content_header_title', 'Kategori')
+@section('content_header_subtitle', 'Create')
+
+@section('content')
+    <div class="container">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Buat Kategori baru</h3>
+            </div>
+
+            <form method="post" action="../kategori">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="kodeKategori">Kode Kategori</label>
+                        <input type="text" name="kodeKategori" class="form-control" id="kodeKategori"
+                            placeholder="Masukkan Kode Kategori">
+                    </div>
+                    <div class="form-group">
+                        <label for="namaKategori">Nama Kategori</label>
+                        <input type="text" name="namaKategori" class="form-control" id="namaKategori"
+                            placeholder="Masukkan Nama Kategori">
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
