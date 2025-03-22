af4ec6f9bedd057011bc6f146f34ba3d85c2cb19
fateehhh <siyaapppp@gmail.com>
2025-03-23 00:07:10 +0700

    JS5.2_Tugas Praktikum

---
+++ Week 5 - Baru/PWL_POS/resources/views/kategori/index.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.template')
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if(session('success'))
+                <div class="alert alert-success alert-dismissible">
+                    <h5><i class="icon fas fa-check"></i> Success!</h5>
+                    {{ session('success') }}
+                </div>
+            @endif
+
+            @if(session('error'))
+                <div class="alert alert-danger alert-dismissible">
+                    <h5><i class="icon fas fa-ban"></i> Error!</h5>
+                    {{ session('error') }}
+                </div>
+            @endif
+            <div class="row mx-3 mt-2">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select class="form-control" id="kategori_kode" name="kategori_kode">
+                                <option value="">- Semua -</option>
+                                @foreach ($kategori as $item)
+                                    <option value="{{ $item->kategori_kode }}">{{ $item->kategori_kode }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Kode Kategori</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_kategori">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode</th>
+                        <th>Nama Kategori</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+@endsection
+@push('css')
+@endpush
+@push('js')
+    <script>
+        $(document).ready(function () {
+            var dataLevel = $('#table_kategori').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('kategori/list') }}",
+                    type: "POST",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: function (d) {
+                        d.kategori_kode = $('#kategori_kode').val(); // Kirim nilai filter kategori_kode
+                    }
+                },
+                columns: [
+                    {
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false,
+                    },
+                    {
+                        data: "kategori_kode",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "kategori_nama",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "aksi",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+
+            // Event listener untuk filter kategori
+            $('#kategori_kode').on('change', function () {
+                dataLevel.ajax.reload();
+            });
+        });
+
+    </script>
+@endpush
--- Week 5 - Baru/PWL_POS/routes/web.php
+++ Week 5 - Baru/PWL_POS/routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KetegoriController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +24,7 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KetegoriController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
@@ -34,7 +36,7 @@ Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 Route::get('/', [WelcomeController::class, 'index']);
 
 // User
-Route::group(['prefix' => 'user'],function () {
+Route::group(['prefix' => 'user'], function () {
     Route::get('/', [UserController::class, 'index']);           // menampilkan halaman awal user
     Route::post('/list', [UserController::class, 'list']);       // menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
@@ -43,4 +45,52 @@ Route::group(['prefix' => 'user'],function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']);   // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
+});
+
+// Level
+Route::prefix('level')->group(function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}', [LevelController::class, 'show']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
+});
+
+// Kategori
+Route::prefix('kategori')->group(function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+    Route::post('/list', [KategoriController::class, 'list']);
+});
+
+// Supplier
+Route::group(['prefix' => 'supplier'], function () {
+    Route::get('/', [SupplierController::class, 'index']);
+    Route::post('/list', [SupplierController::class, 'list']);
+    Route::get('/create', [SupplierController::class, 'create']);
+    Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/{id}', [SupplierController::class, 'show']);
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);
+    Route::put('/{id}', [SupplierController::class, 'update']);
+    Route::delete('/{id}', [SupplierController::class, 'destroy']);
+});
+
+// Barang
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
 });
