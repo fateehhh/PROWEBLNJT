bef08229c5e3c0e0d2d288f7950f9793efd60d4f
fateehhh <siyaapppp@gmail.com>
2025-03-23 11:16:34 +0700

    JS6_Praktikum 3 - Modal Ajax Hapus Data (Data User)

--- Week 6/PWL_POS/routes/web.php
+++ Week 6/PWL_POS/routes/web.php
@@ -42,12 +42,15 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']);          // menyimpan data user baru
     Route::get('/create_ajax', [UserController::class, 'create_ajax']);
-     Route::post('/ajax', [UserController::class, 'store_ajax']);
+    Route::post('/ajax', [UserController::class, 'store_ajax']);
     Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
+    Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);
     Route::get('/{id}/edit', [UserController::class, 'edit']);   // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 });
 
