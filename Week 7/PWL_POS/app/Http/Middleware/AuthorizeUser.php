b4621b555745f6b92494ca0a03a1e5dce95525ab
fateehhh <siyaapppp@gmail.com>
2025-04-07 15:48:21 +0700

    JS3_Praktikum 3 – Implementasi Multi-Level Authorizaton di Laravel dengan Middleware

--- Week 7/PWL_POS/app/Http/Middleware/AuthorizeUser.php
+++ Week 7/PWL_POS/app/Http/Middleware/AuthorizeUser.php
@@ -13,12 +13,11 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ... $roles): Response
     {
-        $user = $request->user();
-
-        if ($user->hasRole($role)) {
-            return $next($request);
+        $user_role = $request->user()->getRole();               //digunakan untuk mengambil data level_kode dari user saat login
+        if (in_array($user_role, $roles)) {   //memeriksa apakah level_kode user saat login ada dalam array $roles
+            return $next($request);                             //jika ada, maka jalankan $next
         }
         abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
--- Week 7/PWL_POS/routes/web.php
+++ Week 7/PWL_POS/routes/web.php
@@ -58,6 +58,24 @@ Route::middleware(['auth'])->group(function () { //artinya semua route di dalam 
 
     });
 
+    //artinya semua route di dalam group ini harus punya role ADM (Administrator) dan MNG (Manajer)
+    Route::middleware(['authorize:ADM,MNG'])->group(function () {
+        Route::get('/barang', [BarangController::class, 'index']);
+        Route::post('/barang/list', [BarangController::class, 'list']);
+        Route::get('/barang/create', [BarangController::class, 'create']);
+        Route::post('/barang/', [BarangController::class, 'store']);
+        Route::get('/barang/create_ajax', [BarangController::class, 'create_ajax']);
+        Route::post('/barang/ajax', [BarangController::class, 'store_ajax']);
+        Route::get('/barang/{id}', [BarangController::class, 'show']);
+        Route::get('/barang/{id}/show_ajax', [BarangController::class, 'show_ajax']);
+        Route::get('/barang/{id}/edit', [BarangController::class, 'edit']);
+        Route::put('/barang/{id}', [BarangController::class, 'update']);
+        Route::get('/barang/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
+        Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']);
+        Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
+        Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+        Route::delete('/barang/{id}', [BarangController::class, 'destroy']);
+    });
 });
 
 // User
@@ -137,20 +155,20 @@ Route::group(['prefix' => 'supplier'], function () {
 });
 
 // Barang
-Route::group(['prefix' => 'barang'], function () {
-    Route::get('/', [BarangController::class, 'index']);
-    Route::post('/list', [BarangController::class, 'list']);
-    Route::get('/create', [BarangController::class, 'create']);
-    Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
-    Route::post('/ajax', [BarangController::class, 'store_ajax']);
-    Route::post('/', [BarangController::class, 'store']);
-    Route::get('/{id}', [BarangController::class, 'show']);
-    Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
-    Route::get('/{id}/edit', [BarangController::class, 'edit']);
-    Route::put('/{id}', [BarangController::class, 'update']);
-    Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
-    Route::delete('/{id}', [BarangController::class, 'destroy']);
-});
+// Route::group(['prefix' => 'barang'], function () {
+//     Route::get('/', [BarangController::class, 'index']);
+//     Route::post('/list', [BarangController::class, 'list']);
+//     Route::get('/create', [BarangController::class, 'create']);
+//     Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
+//     Route::post('/ajax', [BarangController::class, 'store_ajax']);
+//     Route::post('/', [BarangController::class, 'store']);
+//     Route::get('/{id}', [BarangController::class, 'show']);
+//     Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
+//     Route::get('/{id}/edit', [BarangController::class, 'edit']);
+//     Route::put('/{id}', [BarangController::class, 'update']);
+//     Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
+//     Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
+//     Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
+//     Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+//     Route::delete('/{id}', [BarangController::class, 'destroy']);
+// });
