f8068de1cbca8b74d361b8579df7acf1d00f0bcc
fateehhh <siyaapppp@gmail.com>
2025-04-14 02:57:14 +0700

    PWL_POS - Fix tampilan Login dan Registrasi

--- PWL_POS/resources/views/auth/login.blade.php
+++ PWL_POS/resources/views/auth/login.blade.php
@@ -3,147 +3,170 @@
 
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Pengguna</title>
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <title>Login</title>
+
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+    <!-- Font & Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .container {
+            margin: 50px auto;
+            width: 100%;
+            max-width: 720px;
+            padding: 20px;
+        }
+
+        .body {
+            display: flex;
+            position: relative;
+            width: 720px;
+            height: 720px;
+            margin: 20px auto;
+            border: 1px solid #dddd;
+            border-radius: 18px;
+            overflow: hidden;
+            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        }
+
+        .box-1 img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .box-2 {
+            padding: 30px;
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+
+        .box-1,
+        .box-2 {
+            width: 50%;
+        }
+
+        .h-1 {
+            font-size: 24px;
+            font-weight: 700;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-top: 8px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+
+        .btn {
+            background-color: rgb(57, 104, 114);
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+
+        .btn:hover {
+            background-color: #294c53;
+        }
+
+        .error-text {
+            font-size: 12px;
+            color: red;
+        }
+
+        .back {
+            color: #294c53;
+            margin-top: auto;
+            font-size: 14px;
+            text-align: center;
+        }
+
+        @media (max-width:767px) {
+            .body {
+                flex-direction: column;
+                width: 100%;
+                height: auto;
+            }
+
+            .box-1,
+            .box-2 {
+                width: 100%;
+                height: auto;
+            }
+        }
+    </style>
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center"><a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a></div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+<body>
+    <div class="container">
+        <div class="body">
+            <div class="box-1">
+                <img src="https://images.pexels.com/photos/1059979/pexels-photo-1059979.jpeg" alt="">
+            </div>
+            <div class="box-2">
+                <p class="h-1 mb-4">Login Akun</p>
                 <form action="{{ url('login') }}" method="POST" id="form-login">
                     @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" id="username" name="username" class="form-control" placeholder="Username">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                        <small id="error-username" class="error-text text-danger"></small>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" id="password" name="password" class="form-control"
-                            placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                        <small id="error-password" class="error-text text-danger"></small>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember"><label for="remember">Remember Me</label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                    <div class="mt-3 text-center">
-                        <a href="{{ url('register') }}">Belum punya akun? Daftar di sini</a>
-                    </div>
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="Masukkan username">
+                    <small id="error-username" class="error-text"></small>
+
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Masukkan password">
+                    <small id="error-password" class="error-text"></small>
+
+                    <button type="submit" class="btn mt-1">Login</button>
                 </form>
+                <a href="{{ url('/register') }}" class="back">Belum punya akun? Daftar di sini</a>
             </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.login-box -->
 
-    <!-- jQuery -->
     <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- jquery-validation -->
-    <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-    <script src="{{ asset('adminlte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
-    <!-- SweetAlert2 -->
     <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
-
     <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
         $(document).ready(function () {
-            $("#form-login").validate({
-                rules: {
-                    username: {
-                        required: true,
-                        minlength: 4,
-                        maxlength: 20
-                    },
-                    password: {
-                        required: true,
-                        minlength: 5,
-                        maxlength: 20
-                    }
-                },
-                submitHandler: function (form) { // ketika valid, maka bagian yg akan dijalankan 
-                    $.ajax({
-                        url: form.action,
-                        type: form.method,
-                        data: $(form).serialize(),
-                        success: function (response) {
-                            if (response.status) { // jika sukses 
-                                Swal.fire({
-                                    icon: 'success',
-                                    title: 'Berhasil',
-                                    text: response.message,
-                                }).then(function () {
-                                    window.location = response.redirect;
-                                });
-                            } else { // jika error 
-                                $('.error-text').text('');
-                                $.each(response.msgField, function (prefix, val) {
-                                    $('#error-' + prefix).text(val[0]);
-                                });
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Terjadi Kesalahan',
-                                    text: response.message
-                                });
-                            }
+            $('#form-login').on('submit', function (e) {
+                e.preventDefault();
+                $.ajax({
+                    url: $(this).attr('action'),
+                    type: 'POST',
+                    data: $(this).serialize(),
+                    success: function (res) {
+                        if (res.status) {
+                            Swal.fire('Berhasil', res.message, 'success').then(() => {
+                                window.location.href = res.redirect;
+                            });
+                        } else {
+                            $('.error-text').text('');
+                            $.each(res.msgField, function (prefix, val) {
+                                $('#error-' + prefix).text(val[0]);
+                            });
+                            Swal.fire('Gagal', res.message, 'error');
                         }
-                    });
-                    return false;
-                },
-                errorElement: 'span',
-                errorPlacement: function (error, element) {
-                    error.addClass('invalid-feedback');
-                    element.closest('.input-group').append(error);
-                },
-                highlight: function (element, errorClass, validClass) {
-                    $(element).addClass('is-invalid');
-                },
-                unhighlight: function (element, errorClass, validClass) {
-                    $(element).removeClass('is-invalid');
-                }
+                    }
+                });
             });
         });
     </script>
--- PWL_POS/resources/views/auth/register.blade.php
+++ PWL_POS/resources/views/auth/register.blade.php
@@ -5,75 +5,167 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registrasi Pengguna</title>
+    <title>Register</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+    <!-- Font & Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif
+        }
+
+        .container {
+            margin: 50px auto;
+            width: 100%;
+            max-width: 720px;
+            padding: 20px;
+        }
+
+        .body {
+            display: flex;
+            /* Tambahan ini penting */
+            position: relative;
+            width: 720px;
+            height: 720px;
+            margin: 20px auto;
+            border: 1px solid #dddd;
+            border-radius: 18px;
+            overflow: hidden;
+            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        }
+
+
+        .box-1 img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .box-2 {
+            padding: 30px;
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+        
+        .box-1,
+        .box-2 {
+            width: 50%;
+        }
+
+        .h-1 {
+            font-size: 24px;
+            font-weight: 700;
+        }
+
+        .text-muted {
+            font-size: 14px;
+        }
+
+        input,
+        select {
+            width: 100%;
+            padding: 10px;
+            margin-top: 8px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+
+        .btn {
+            background-color: rgb(57, 104, 114);
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+
+        .btn:hover {
+            background-color: #294c53;
+        }
+
+        .error-text {
+            font-size: 12px;
+            color: red;
+        }
+
+        .back {
+            color: #294c53;
+            margin-top: auto;
+            font-size: 14px;
+            text-align: center;
+        }
+
+        .footer {
+            font-size: 10px;
+        }
+
+        @media (max-width:767px) {
+            .body {
+                flex-direction: column;
+                width: 100%;
+                height: auto;
+            }
+
+            .box-1,
+            .box-2 {
+                width: 100%;
+                height: auto;
+            }
+        }
+    </style>
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a>
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Register akun baru</p>
-
-                <form action="{{ url('/register') }}" method="POST" id="form-register">
-                    @csrf
-                    <div class="form-group">
+<body>
+    <div class="container">
+        <div class="body d-md-flex align-items-center justify-content-between">
+            <div class="box-2 d-flex flex-column h-100">
+                <div>
+                    <p class="mb-4 h-1">Buat Akun</p><br>
+                    <form action="{{ url('/register') }}" method="POST" id="form-register">
+                        @csrf
                         <label>Level Pengguna</label>
-                        <select name="level_id" class="form-control">
+                        <select name="level_id">
                             <option value="">- Pilih Level -</option>
                             @foreach($level as $l)
                                 <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
                             @endforeach
                         </select>
-                        <small id="error-level_id" class="text-danger error-text"></small>
-                    </div>
+                        <small id="error-level_id" class="error-text"></small>
 
-                    <div class="form-group">
                         <label>Username</label>
-                        <input name="username" class="form-control">
-                        <small id="error-username" class="text-danger error-text"></small>
-                    </div>
+                        <input name="username" placeholder="Masukkan username">
+                        <small id="error-username" class="error-text"></small>
 
-                    <div class="form-group">
                         <label>Nama</label>
-                        <input name="nama" class="form-control">
-                        <small id="error-nama" class="text-danger error-text"></small>
-                    </div>
+                        <input name="nama" placeholder="Masukkan nama lengkap">
+                        <small id="error-nama" class="error-text"></small>
 
-                    <div class="form-group">
                         <label>Password</label>
-                        <input name="password" type="password" class="form-control">
-                        <small id="error-password" class="text-danger error-text"></small>
-                    </div>
+                        <input name="password" type="password" placeholder="Buat password">
+                        <small id="error-password" class="error-text"></small>
 
-                    <div class="form-group">
                         <label>Konfirmasi Password</label>
-                        <input name="password_confirmation" type="password" class="form-control">
-                    </div>
+                        <input name="password_confirmation" type="password" placeholder="Ulangi password">
 
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="{{ url('/login') }}">Sudah punya akun?</a>
-                        </div>
-                        <div class="col-6">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button>
-                        </div>
-                    </div>
-                </form>
+                        <button type="submit" class="btn mt-2">Daftar</button>
+                    </form>
+                </div>
+                <a href="{{ url('/login') }}" class="back text-center mt-auto"> sudah punya akun? Log in</a>
+            </div>
+            <div class="box-1 mt-md-0 mt-5">
+                <img src="https://images.pexels.com/photos/1059979/pexels-photo-1059979.jpeg" alt="">
             </div>
         </div>
     </div>
@@ -89,7 +181,6 @@
     <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
-
     <script>
         $(document).ready(function () {
             $('#form-register').on('submit', function (e) {
