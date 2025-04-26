2c991acfb19ff66eba9f72011783062a5115444a
fateehhh <siyaapppp@gmail.com>
2025-04-27 00:04:12 +0700

    JS11_Tugas Praktikum (add image for m_barang)

--- Week 11/PWL_POS/app/Models/BarangModel.php
+++ Week 11/PWL_POS/app/Models/BarangModel.php
@@ -4,17 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
 
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
-    protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+    protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'image', 'harga_beli', 'harga_jual'];
 
     public function kategori()
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => asset('storage/barang/' . $image),
+        );
     }
 
     public function stok()
